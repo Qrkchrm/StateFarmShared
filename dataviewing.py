b1824c44046b561a77e68c97708ec98ccae494d0
Daniel <daniel@daniel-pc.home>
2016-06-17 21:30:18 -0700

    Initial commit

---
+++ dataviewing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import skimage
+import sklearn
+import pylab as plt
+import os
+
+def pickle_dataset(datadir):
+    for i in os.listdir()
+        pass
+
+
+class DataViewer:
+    def __init__self(self):
+        pass
