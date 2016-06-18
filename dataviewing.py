fd21fa5b5205a39ed134ba06867356f4c27325f3
daniel <zellerdan@gmail.com>
2016-06-17 22:09:48 -0700

    Created function to resize and grayscale images.

--- dataviewing.py
+++ dataviewing.py
@@ -1,13 +1,25 @@
 import pandas as pd
-import skimage
+import skimage,skimage.io,skimage.color,skimage.transform
 import sklearn
 import pylab as plt
 import os
+import shutil
+import scipy.misc
+from scipy import ndimage
+import numpy as np
 
-def pickle_dataset(datadir):
-    for i in os.listdir()
-        pass
+def resize_dataset():
+    for label in os.listdir("Data/imgs/train"):
+        for img in os.listdir("Data/imgs/train/"+label):
+            print(img)
+            im=skimage.io.imread("Data/imgs/train/"+label+"/"+img)
+            im=skimage.color.rgb2gray(im)
+            im=skimage.transform.resize(im,(64,64))
+            if not os.path.exists("Data/imgs_small/train/"+label):
+                os.makedirs("Data/imgs_small/train/"+label)
+            skimage.io.imsave("Data/imgs_small/train/"+label+"/"+img,im)
 
+resize_dataset()
 
 class DataViewer:
     def __init__self(self):
