72c0a53f9a3f86af423626b84b94b847eb2e2720
daniel <zellerdan@gmail.com>
2016-06-17 22:22:07 -0700

    Created function to resize and grayscale images.

--- dataviewing.py
+++ dataviewing.py
@@ -11,16 +11,20 @@ import numpy as np
 def resize_dataset():
     for label in os.listdir("Data/imgs/train"):
         for img in os.listdir("Data/imgs/train/"+label):
-            print(img)
-            im=skimage.io.imread("Data/imgs/train/"+label+"/"+img)
-            im=skimage.color.rgb2gray(im)
-            im=skimage.transform.resize(im,(64,64))
-            if not os.path.exists("Data/imgs_small/train/"+label):
-                os.makedirs("Data/imgs_small/train/"+label)
-            skimage.io.imsave("Data/imgs_small/train/"+label+"/"+img,im)
-
-resize_dataset()
+            if not os.path.exists("Data/imgs_small/train/"+label+"/"+img):
+                try:
+                    im=skimage.io.imread("Data/imgs/train/"+label+"/"+img)
+                    im=skimage.color.rgb2gray(im)
+                    im=skimage.transform.resize(im,(64,64))
+                    if not os.path.exists("Data/imgs_small/train/"+label):
+                        os.makedirs("Data/imgs_small/train/"+label)
+                    skimage.io.imsave("Data/imgs_small/train/"+label+"/"+img,im)
+                except:
+                    print(img)
 
 class DataViewer:
     def __init__self(self):
         pass
+
+if __name__=="__main__":
+    resize_dataset()
