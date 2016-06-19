a2d4631fda8df8dd3cb931fa793684711352d4fe
daniel <zellerdan@gmail.com>
2016-06-19 11:59:42 -0700

    Created function to resize and grayscale images.

--- dataviewing.py
+++ dataviewing.py
@@ -3,6 +3,7 @@ import skimage,skimage.io,skimage.color,skimage.transform
 import sklearn
 import pylab as plt
 import os
+import numpy as np
 #import seaborn as sb
 
 
@@ -31,9 +32,24 @@ def resize_dataset():
             except:
                 print(img)
 
+def convert_to_numpy():
+    data=pd.read_csv("Data/driver_imgs_list.csv")
+    dataset=np.ndarray((len(data),64,64),dtype=np.float32)
+    print(np.shape(dataset))
+    datalabels=np.ndarray((len(data),len(data["classname"].unique())),dtype=np.float32)
+    print(np.shape(datalabels))
+    def to_one_hot(datalabel):
+        out=np.zeros(10,dtype=np.float32)
+        index=int(datalabel.replace("c",""))
+        out[index]=1
+        #print(out)
+    for i,h in data.iterrows():
+        im=skimage.io.imread("Data/imgs_small/train/"+h.classname+"/"+h.img)
+        datalabels[i]=to_one_hot(h.classname)
+        dataset[i]=im
+    np.save("Data/dataset.npy",dataset)
+    np.save("Data/datalabels.npy",datalabels)
+
 class DataViewer:
     def __init__self(self):
         pass
-
-if __name__=="__main__":
-    resize_dataset()
